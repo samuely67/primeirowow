8545b5599e5e3d4497e286e8ee3f6901d5ad6036
unknown <samuellevyleao@gmail.com>
2026-09-01 19:01:04 -0400

    second commit

---
+++ ficha cadastral.py
@@ -0,0 +1,30 @@
+#dados da pessoa
+#int - numero inteiro
+
+idade = 20
+quantidade = 15
+ano = 2005
+
+#float- numero decimal
+altura = 1.75
+preco = 67
+temperatura = 34.7
+
+#str - texto - textos ficam em aspas
+nome = "ana"  
+cidade = "anor londo"
+curso = "tec in IA"
+
+#bool - verdadeiro ou falso 
+aprovado = True
+reprovado = False
+
+#usando as 4 variaveis juntas
+nome = "ana"
+idade = 20
+estudando = "true"
+
+print(nome)
+print(idade)
+print(altura)
+print(estudando)
---
+++ hello_mundo2.py
@@ -0,0 +1,12 @@
+print("ola amigos como estao tranquilos")
+
+nome="samuel"
+idade"19"
+
+estudante="true"
+print(estudante)
+
+print(nome)
+print("minha idade nao e essa, idade")
+
+nome_completo= (input())
