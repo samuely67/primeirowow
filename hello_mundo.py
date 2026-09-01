abc06f3b223ce901ad67c55be0992f0130dd6558
unknown <samuellevyleao@gmail.com>
2026-08-31 20:26:10 -0400

    fist commit

---
+++ hello_mundo.py
@@ -0,0 +1,11 @@
+#declarando e inicializando as variaveis
+nome ="ana"
+idade = 20
+altura = 1.58
+estudante= True
+
+#imprimindo os valores
+print(nome)
+print(idade)
+print(altura)
+print(estudante)
